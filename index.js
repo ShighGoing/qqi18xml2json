566c34550e5c47e0b2773c42e491bfec5fa79c3d
shigh <--global>
2016-11-15 05:46:14 -0800

    add write file code

--- index.js
+++ index.js
@@ -10,19 +10,56 @@ var path = './LocList.xml'
 fs.readFile(path, 'utf8', (err, data) => {
     if (err) throw err;
     parser.on('startElement', (name, attrs) => {
-        //console.log(name, attrs)
-	genCountries(name, attrs)
-        parseEles(0, attrs)
+        // console.log(name, attrs)
+	// genCountries(name, attrs)
+        // parseEles(0, attrs)
+        parseEles2Arr(name, attrs)
     })
     parser.on('endElement', (name) => {
-        //console.log(name)
-        parseEles(1)
-    })
-    parser.on('close', () => {
-        console.log(areas.subs[1].subs)
+        // console.log(name)
+        // parseEles(1)
     })
     parser.end(data)
+    output(areas)
 })
+
+function output (data) {
+    fs.writeFile('./output.json', JSON.stringify(data), (err) => {
+        if (err) throw err
+    })
+}
+
+var curCountry, curState, curCity
+
+function parseEles2Arr (name, attrs) {
+    if (!areas) areas = []
+    if (name === 'CountryRegion') {
+        curCountry = {}
+        if (attrs) {
+            curCountry.name = attrs.Name
+            curCountry.code = attrs.Code
+        }
+        areas.push(curCountry)
+    } else if (name === 'State') {
+        curState = {}
+        if (attrs) {
+            curState.name = attrs.Name
+            curState.code = attrs.Code
+        }
+        if (!curCountry) throw new Error()
+        if (!curCountry.states) curCountry.states = []
+        curCountry.states.push(curState)
+    } else if (name === 'City') {
+        curCity = {}
+        if (attrs) {
+            curCity.name = attrs.Name
+            curCity.code = attrs.Code
+        }
+        if (!curState) throw new Error()
+        if (!curState.citys) curState.citys = []
+        curState.citys.push(curCity)
+    }
+}
 
 function parseEles (type, attrs) {
     if (type === 0) { // start
