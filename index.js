8e04210b8485c77ad573b02ffbf5c65658057fce
ShighGoing <shigh945@gmail.com>
2016-11-16 12:04:20 +0800

    增加文件路径输入识别

--- index.js
+++ index.js
@@ -1,93 +1,64 @@
 const fs = require('fs');
 const expat = require('node-expat')
-
 var parser = new expat.Parser('UTF-8')
-var countryObj = {}
-var countryArr = []
-var namespaces = []
-var areas
-var path = './LocList.xml'
-fs.readFile(path, 'utf8', (err, data) => {
-    if (err) throw err;
-    parser.on('startElement', (name, attrs) => {
-        // console.log(name, attrs)
-	// genCountries(name, attrs)
-        // parseEles(0, attrs)
-        parseEles2Arr(name, attrs)
-    })
-    parser.on('endElement', (name) => {
-        // console.log(name)
-        // parseEles(1)
-    })
-    parser.end(data)
-    output(areas)
-})
 
-function output (data) {
-    fs.writeFile('./output.json', JSON.stringify(data), (err) => {
+function main () {
+    process.argv.forEach((val, index) => {
+        if (index > 1) {
+            parseFile(val)
+        }
+    })
+}
+
+function parseFile (path) {
+    fs.readFile(path, 'utf8', (err, data) => {
+        if (err) throw err;
+        parser.on('startElement', (name, attrs) => {
+            objectStructure.add(name, attrs)
+        })
+        parser.write(data) // sync
+        output(objectStructure.areas)
+    })
+}
+
+function output (path, data) {
+    fs.writeFile('path', JSON.stringify(data), (err) => {
         if (err) throw err
     })
 }
 
-var curCountry, curState, curCity
-
-function parseEles2Arr (name, attrs) {
-    if (!areas) areas = []
-    if (name === 'CountryRegion') {
-        curCountry = {}
-        if (attrs) {
-            curCountry.name = attrs.Name
-            curCountry.code = attrs.Code
+var objectStructure = {
+    areas: null,
+    curCountry: null,
+    curState: null,
+    curCity: null,
+    add: function (name, attrs) {
+        if (!this.areas) this.areas = []
+        if (name === 'CountryRegion') {
+            this.curCountry = {}
+            if (attrs) {
+                this.curCountry.name = attrs.Name
+                this.curCountry.code = attrs.Code
+            }
+            this.areas.push(this.curCountry)
+        } else if (name === 'State') {
+            this.curState = {}
+            if (attrs) {
+                this.curState.name = attrs.Name
+                this.curState.code = attrs.Code
+            }
+            if (!this.curCountry) throw new Error()
+            if (!this.curCountry.states) this.curCountry.states = []
+            curCountry.states.push(curState)
+        } else if (name === 'City') {
+            this.curCity = {}
+            if (attrs) {
+                this.curCity.name = attrs.Name
+                this.curCity.code = attrs.Code
+            }
+            if (!this.curState) throw new Error()
+            if (!this.curState.citys) this.curState.citys = []
+            this.curState.citys.push(curCity)
         }
-        areas.push(curCountry)
-    } else if (name === 'State') {
-        curState = {}
-        if (attrs) {
-            curState.name = attrs.Name
-            curState.code = attrs.Code
-        }
-        if (!curCountry) throw new Error()
-        if (!curCountry.states) curCountry.states = []
-        curCountry.states.push(curState)
-    } else if (name === 'City') {
-        curCity = {}
-        if (attrs) {
-            curCity.name = attrs.Name
-            curCity.code = attrs.Code
-        }
-        if (!curState) throw new Error()
-        if (!curState.citys) curState.citys = []
-        curState.citys.push(curCity)
     }
-}
-
-function parseEles (type, attrs) {
-    if (type === 0) { // start
-        if (!areas) {
-            areas = {}
-            return
-        }
-        let curObj = areas
-        namespaces.forEach((level) => {
-           curObj = curObj.subs[level]
-        })
-        if (!curObj.subs) {
-            curObj.subs = {}
-        }
-        let code = attrs && attrs.Code || '_'
-        curObj.subs[code] = {
-            name: attrs.Name,
-            code: attrs.Code
-        }
-        namespaces.push(attrs && attrs.Code || '_')
-    } else if (type === 1) {
-    	namespaces.pop()
-    }
-}
-
-function genCountries(name, attrs) {
-    if (name !== 'CountryRegion') {
-        return
-    }
-    countryObj[attrs.Code] = attrs
 }
